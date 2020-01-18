f7ac717ad03219a643083aad3bd6b1b216bd3a41
Daniel Li <daniel.li.birds@gmail.com>
2020-01-17 20:06:42 -0500

    Create assignment form uses date input Decreasing size of assignments on admin page

--- classes/create-assignment.js
+++ classes/create-assignment.js
@@ -19,6 +19,19 @@ function init(app) {
   app.post('/:classid/item/new', authMiddleware(), (req, res) => {
     let classID = req.params.classid;
     let classObj = res.locals.classObj;
+    let parsedDate = Math.floor(Date.parse(req.body.duedate) / 1000);
+    if (!(req.body.title) || !(req.body.description) || isNaN(parsedDate) || !(req.body.workload)) {
+      // Empty input
+      console.log('[LOG] create-assignment: Invalid input given');
+      res.redirect('/classes/' + classID + '/item/new?e=1');
+      return;
+    }
+    if ((req.body.description.length > 2000) || (req.body.title.length > 200)
+      || (parsedDate < (Date.now() / 1000)) || (req.body.workload < 0) || (req.body.workload > 100)) {
+      console.log('[LOG] create-assignment: Invalid input given');
+      res.redirect('/classes/' + classID + '/item/new?e=1');
+      return;
+    }
     classDB.enrollStatus(req.user.data.username, classID).then(userStatus => {
       if (!(userStatus.admin) && !(userStatus.enrolled && classObj.post_perm)) {
         console.log('[LOG] create-assignment: No permissions');
@@ -26,22 +39,10 @@ function init(app) {
         res.redirect('/classes/' + classID + '/item/new?e=2');
         return;
       }
-      if (!(req.body.title) || !(req.body.description) || !(req.body.duedate) || !(req.body.workload)) {
-        // Empty input
-        console.log('[LOG] create-assignment: Invalid input given');
-        res.redirect('/classes/' + classID + '/item/new?e=1');
-        return;
-      }
-      if ((req.body.description.length > 2000) || (req.body.title.length > 200)
-        || (req.body.duedate < (Date.now() / 1000)) || (req.body.workload < 0) || (req.body.workload > 100)) {
-        console.log('[LOG] create-assignment: Invalid input given');
-        res.redirect('/classes/' + classID + '/item/new?e=1');
-        return;
-      }
       let assignObj = {
         title: req.body.title,
         description: req.body.description,
-        expires: req.body.duedate,
+        expires: parsedDate,
         workload: req.body.workload
       }
       let assignRef = assignDB.createAssignment(classID, assignObj);
--- public/js/create-assignment-jquery.js
+++ public/js/create-assignment-jquery.js
@@ -3,18 +3,21 @@ $(document).ready(function() {
   var $output = $('#worknum');
   // Update the current slider value (each time you drag the slider handle)
   $slider.on('input', function() {
-    $output.text($(this).val());
+    let $this = $(this)
+    var rangePercent = $this.val();
+    $output.text(rangePercent);
+    $this.css('filter', 'hue-rotate(' + rangePercent*0.6 + 'deg)');
   });
 
-  $('assignment-form').on('click', '[click-editable]', function( event ) {
+  $('.assignment-form').on('click', '[click-editable]', function( event ) {
     var $this = $(this);
-    var $input = $('<input class="num-input" pattern="[0-9]+" />').val($this.text());
+    var $input = $('<input class="num-input" maxlength="3" pattern="[0-9]+" />').val($this.text());
     $input.attr('data-default', $this.text());
     $this.replaceWith($input);
     
     $input.one('blur', function(){
       var newtext = /^[0-9]+$/.test($input.val()) ? $input.val() : $input.attr('data-default');
-      var $textbox = $('<p click-editable />').html('<span id="worknum">'+newtext+'</span>');
+      var $textbox = $('<p class="num-display" id="worknum" click-editable/>').text(newtext);
       $input.replaceWith($textbox);
       $slider.val(newtext)
       $output = $('#worknum');
@@ -22,12 +25,28 @@ $(document).ready(function() {
     $input.focus();
   })
 
-  $('#duedate').val(Math.floor(Date.now() / 1000))
   $('#submit-form').on('click', function() {
-    $('#form').submit();
+    $form = $('#form')
+    if ($form.valid()) {
+      $form.submit();
+    }
   })
 
   $('#description').on('input', function() {
     $('#charcount').text(2000 - $(this).val().length)
   })
+
+  let curTime = new Date();
+  let curTimeString = (new Date(curTime.getTime() - curTime.getTimezoneOffset()*60000)).toISOString().substring(0, 16)
+  $('#duedate').attr('min', curTimeString).val(curTimeString)
+
+  var query = window.location.search.substring(1);
+  var queryObj = {}
+  query.split("&").forEach(queryVal => {
+    var kvPair = queryVal.split("=");
+    queryObj[kvPair[0]] = kvPair[1];
+  })
+  var errnum = queryObj['e'];
+  if (errnum == 1) { $('#errormessage').text("Invalid input.") }
+  if (errnum == 2) { $('#errormessage').text("You do not have the necessary permissions to perform this action.") }
 });
